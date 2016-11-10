5fc8e882e5c85c8c29364604a47ffb5b6b032fc9
Natalia-scr <natalia.scurtu@mail.ru>
2016-11-10 12:26:22 +0000

    add mauro's code along

---
+++ src/components/TrendsBox.js
@@ -0,0 +1,21 @@
+const React = require('react');
+
+const TrendsBox = React.createClass({
+  render: function () {
+    return (
+      <div className="box">
+        <h2 className="title is-1">Trend Box</h2>
+        <ul className="trendList">
+          {this.props.trends.slice(0,10).map(function(trend, i) {
+            return (
+              <li className="trend" key={i}><a href ={trend.url}> {trend.name}</a></li>
+            )
+          })
+          }
+        </ul>
+      </div>
+    );
+  }
+});
+
+module.exports = TrendsBox;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const TrendsBox = require('./components/TrendsBox');
+const request = require('superagent');
+
 
 const App = React.createClass({
+
+  getInitialState: function (){
+    return {
+      trends:[]
+    }
+  },
+
+  componentWillMount: function () {
+    request
+    .get('https://protected-oasis-31937.herokuapp.com/trends')
+    .end(function(error, response) {
+      if(error) {
+        console.log(error);
+      }
+    this.setState({
+        trends:response.body.tweetData.trends[0].trends
+    });
+      console.log(response.body.tweetData.trends[0].trends);
+    }.bind(this));
+
+  },
+
   render: function () {
     return (
       <div>
         <h1 className="title is-1">Northwitter</h1>
+        <TrendsBox trends = {this.state.trends}/>
       </div>
     );
   }
